68de7a4a599e0010804f772ed790653cf90b4024
Youcef B <ybena102@uottawa.ca>
2019-11-03 17:31:57 -0500

    Added more animations, the background for the features page is now dynamic

--- features.js
+++ features.js
@@ -1,1 +1,11 @@
-var x = 1;
+var Pictures = ["url('Wallpaper1.jpg')", "url('Wallpaper2.jpg')", "url('Wallpaper3.jpg')", "url('Wallpaper4.jpg')", "url('Wallpaper5.jpg')"]
+
+function ChangeBg() {
+    var rand = Pictures[Math.floor(Math.random() * Pictures.length)];
+    document.body.style.transition = "all 3.5s";
+    document.body.style.backgroundImage = rand
+}
+
+window.setInterval(function(){
+    ChangeBg()
+  }, 3000);
